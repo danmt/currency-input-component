f46d9f5fec1b4cec9552fbf52ce658dc92787ceb
danmt <danielarturomt@gmail.com>
2019-11-27 17:32:58 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# currency-input-component
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/currency-input-component)
---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.scss' ]
+})
+export class AppComponent  {
+  amountCtrl = new FormControl(null, { updateOn: 'blur' });
+}
